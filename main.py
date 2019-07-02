0c92fe9a19df7a63532155852e66762e4216cb38
Kai <hamidghadery22@gmail.com>
2019-07-02 22:14:22 +0430

    new qt app

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(800, 600)
+    w.move(0, 0)
+    w.setWindowTitle('Simple')
+    w.show()
+    
+    sys.exit(app.exec_())
